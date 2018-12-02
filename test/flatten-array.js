09b2d04754ab570d859fe1f1580e516a74de9f2c
NadaMohsen <45532921+NadaMohsen@users.noreply.github.com>
2018-12-02 13:18:39 +0200

    Update flatten-array.js

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,8 +1,20 @@
 describe('flatten array', function () {
-  it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
-
+         const flatten = function(arr, result = []) {
+          for (let i = 0, length = arr.length; i < length; i++) {
+            const value = arr[i];
+            if (Array.isArray(value)) {
+              flatten(value, result);
+            } else {
+              result.push(value);
+            }
+          }
+          return result;
+        };
+        var newArray = flatten(arr);
+       arr = newArray.sort((a, b) => a - b);
+      
     expect(arr).toEqual(expected);
   });
 });
